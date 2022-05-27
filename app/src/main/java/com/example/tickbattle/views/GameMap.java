dab8921417e08759d885bf6bfbfbe604d103bbfb
Anton Naumtsev <naumtsevanton@mail.ru>
2022-05-28 02:30:25 +0300

    fix view for block

--- app/src/main/java/com/example/tickbattle/views/GameMap.java
+++ app/src/main/java/com/example/tickbattle/views/GameMap.java
@@ -2,24 +2,29 @@ package com.example.tickbattle.views;
 
 import android.content.Context;
 import android.graphics.Color;
-import android.view.Gravity;
+import android.graphics.Typeface;
 import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.GridLayout;
-import android.widget.RelativeLayout;
-import android.view.ViewGroup.LayoutParams;
-import android.widget.ScrollView;
-import android.widget.TableLayout;
-import android.widget.TableRow;
-;import ua.org.tenletters.widget.DiagonalScrollView;
+import android.widget.TextView;
+;import androidx.annotation.NonNull;
+import androidx.core.content.res.ResourcesCompat;
+
+import com.example.tickbattle.R;
+
+import ua.org.tenletters.widget.DiagonalScrollView;
+
+enum DIRECTION {
+    LEFT,
+    RIGHT,
+    UP,
+    DOWN
+}
+
 
 public class GameMap extends DiagonalScrollView {
-    private float startX;
-    private float startY;
-    private int startPointerID;
     GridLayout grid;
     private float currentScale = 1f;
     private final ScaleGestureDetector scaleDetector;
@@ -34,7 +39,7 @@ public class GameMap extends DiagonalScrollView {
         scaleDetector = new ScaleGestureDetector(context, new ScaleListener());
 
 
-        int w = 15;
+        int w = 10;
         int h = 10;
 
         grid = new GridLayout(this.getContext());
@@ -42,9 +47,9 @@ public class GameMap extends DiagonalScrollView {
         grid.setRowCount(w);
         grid.setUseDefaultMargins(true);
 
-        for(int i = 0; i < h; i += 1) {
-            for(int j = 0; j < w; j += 1) {
-                grid.addView(new ExtendedButton(grid.getContext()));
+        for (int i = 0; i < h; i += 1) {
+            for (int j = 0; j < w; j += 1) {
+                grid.addView(new Block(grid.getContext(), j, i));
             }
         }
 
@@ -52,78 +57,171 @@ public class GameMap extends DiagonalScrollView {
     }
 
 
-
-
     @Override
     public boolean dispatchTouchEvent(MotionEvent event) {
-
-        final int X = (int) event.getRawX();
-        final int Y = (int) event.getRawY();
-        int pointerIndex = event.getActionIndex();
-        int pointerID = event.getPointerId(pointerIndex);
         scaleDetector.onTouchEvent(event);
-
-
-        switch (event.getAction() & MotionEvent.ACTION_MASK) {
-
-            case MotionEvent.ACTION_DOWN: // первое касание
-                this.startX = X;
-                this.startY = Y;
-                this.startPointerID = pointerID;
-                System.out.println("DOWN" + String.valueOf(event.getX()));
-                break;
-            case MotionEvent.ACTION_MOVE:
-                if (pointerID != this.startPointerID) break;
-                float coef = 0.1f;
-                float dx = (X - startX);
-                float dy = (Y - startY);
-
-                if( Math.abs(dx) <= 80 && Math.abs(dy) <= 80) break;
-
-                float new_x = grid.getX() + dx * coef;
-                float new_y = grid.getY() + dy * coef;
-
-                grid.setX(new_x);
-                grid.setY(new_y);
-                break;
-        }
         super.dispatchTouchEvent(event);
         return true;
     }
 
+    class Block extends FrameLayout {
+        private int BLOCK_SIZE = 200;
+        private int x;
+        private int y;
+
+        private ExtendedButton btn;
+        private TextView units;
+
+        private TextView leftArrow;
+        private TextView rightArrow;
+        private TextView upArrow;
+        private TextView downArrow;
 
 
-    class ExtendedButton extends androidx.appcompat.widget.AppCompatButton implements View.OnClickListener {
-        int s = 0;
-        public ExtendedButton(Context context) {
+        public Block(@NonNull Context context, int x, int y) {
             super(context);
-            setSoundEffectsEnabled(false);
-            setWidth(200);
-            setHeight(200);
-            setBackgroundColor(Color.YELLOW);
-            setOnClickListener(this);
+            this.x = x;
+            this.y = y;
+
+            btn = new ExtendedButton(this.getContext());
+
+            Typeface typeface = ResourcesCompat.getFont(getContext(), R.font.fa_thin_100);
+
+            btn.setTypeface(typeface);
+            btn.setText(String.valueOf(btn.getHeight()));
+
+            units = new TextView(btn.getContext());
+            units.setTextSize(15);
+            units.setTypeface(typeface);
+
+
+            LayoutParams lp_text = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+            lp_text.setMargins(10, 0, 0, 0);
+            units.setLayoutParams(lp_text);
+            units.setElevation(1000);
+
+            leftArrow = new TextView(btn.getContext());
+            leftArrow.setText("\uF060");
+            leftArrow.setTypeface(typeface);
+            leftArrow.setElevation(1000);
+            leftArrow.setY(BLOCK_SIZE / 2f - 20);
+            leftArrow.setX(20);
+            leftArrow.setVisibility(View.INVISIBLE);
+
+            rightArrow = new TextView(btn.getContext());
+            rightArrow.setText("\uF061");
+            rightArrow.setTypeface(typeface);
+            rightArrow.setElevation(1000);
+            rightArrow.setY(BLOCK_SIZE / 2f - 20);
+            rightArrow.setX(BLOCK_SIZE + 6);
+            rightArrow.setVisibility(View.INVISIBLE);
+
+            upArrow = new TextView(btn.getContext());
+            upArrow.setText("\uF062");
+            upArrow.setTypeface(typeface);
+            upArrow.setElevation(1000);
+            upArrow.setY(5);
+            upArrow.setX(BLOCK_SIZE / 2f + 14);
+            upArrow.setVisibility(View.INVISIBLE);
+
+            downArrow = new TextView(btn.getContext());
+            downArrow.setText("\uF063");
+            downArrow.setTypeface(typeface);
+            downArrow.setElevation(1000);
+            downArrow.setY(BLOCK_SIZE - 55);
+            downArrow.setX(BLOCK_SIZE / 2f + 14);
+            downArrow.setVisibility(View.INVISIBLE);
+
+            addView(btn);
+            addView(leftArrow);
+            addView(rightArrow);
+            addView(upArrow);
+            addView(downArrow);
+            addView(units);
         }
 
-        @Override
-        public void onClick(View v) {
-            s += 1;
-            setText(String.valueOf(s));
+        void setBlockText(@NonNull String text) {
+            btn.setText(text);
+        }
+
+        void setUnitText(@NonNull String text) {
+            units.setText(text);
+        }
+
+        void setBlockColor(int color) {
+            btn.setBackgroundColor(color);
+        }
+
+        private void processClick() {
+            // process (x, y)
+        }
+
+
+        public void setVisableRightArrow(boolean visable) {
+            if (visable) {
+                rightArrow.setVisibility(View.VISIBLE);
+            } else {
+                rightArrow.setVisibility(View.INVISIBLE);
+            }
+        }
+
+        public void setVisableLeftArrow(boolean visable) {
+            if (visable) {
+                leftArrow.setVisibility(View.VISIBLE);
+            } else {
+                leftArrow.setVisibility(View.INVISIBLE);
+            }
+        }
+
+        public void setVisableUpArrow(boolean visable) {
+            if (visable) {
+                leftArrow.setVisibility(View.VISIBLE);
+            } else {
+                leftArrow.setVisibility(View.INVISIBLE);
+            }
+        }
+
+        public void setVisableDownArrow(boolean visable) {
+            if (visable) {
+                downArrow.setVisibility(View.VISIBLE);
+            } else {
+                downArrow.setVisibility(View.INVISIBLE);
+            }
+        }
+
+
+        class ExtendedButton extends androidx.appcompat.widget.AppCompatButton implements View.OnClickListener {
+            public ExtendedButton(Context context) {
+                super(context);
+
+                setSoundEffectsEnabled(false);
+                setWidth(BLOCK_SIZE);
+                setHeight(BLOCK_SIZE);
+                setBackgroundColor(Color.YELLOW);
+                setTextSize(25);
+                setTextAlignment(TEXT_ALIGNMENT_GRAVITY);
+                setOnClickListener(this);
+            }
+
+            @Override
+            public void onClick(View v) {
+                Block.this.processClick();
+            }
         }
     }
+
 
     private class ScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
         @Override
         public boolean onScale(ScaleGestureDetector detector) {
             float scaleFactor = Math.abs(detector.getScaleFactor());
-            if(currentScale * scaleFactor < 0.3) return true;
-            if(currentScale * scaleFactor > 1.5) return true;
+            if (currentScale * scaleFactor < 0.3) return true;
+            if (currentScale * scaleFactor > 1.5) return true;
 
             currentScale *= scaleFactor;
             grid.setScaleX(currentScale);
             grid.setScaleY(currentScale);
-            System.out.println("SCALE: " + String.valueOf(currentScale));
             return true;
         }
     }
-
 }
